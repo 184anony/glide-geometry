17df4126fb138001bca9f27cb09bd71e1ecd040c
184anony <49863944+184anony@users.noreply.github.com>
2023-03-25 06:55:10 +0900

    Update function.js

--- function.js
+++ function.js
@@ -3,13 +3,11 @@ window.function = function (address) {
   address = address.value;
 
   let url = 'https://msearch.gsi.go.jp/address-search/AddressSearch?q=' + address;
-
-  return url;
   
-//   fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-//       let text = Json.parse(data)
-//       return text;
-//     });
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      let text = Json.parse(data)
+      return text;
+    });
 }

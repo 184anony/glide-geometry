e9d1a27396df4a558993a7233165c62ff964cb87
184anony <49863944+184anony@users.noreply.github.com>
2023-03-25 15:16:55 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,13 +1,14 @@
+import fetch from "node-fetch"
+
 window.function = function (address) {
 
   address = address.value;
 
   let url = 'https://msearch.gsi.go.jp/address-search/AddressSearch?q=' + address;
   
-  let request = new XMLHttpRequest();
-  request.open("GET", url);
-  request.send();
-  request.addEventListener("load", function () {
-    return this.responseText;
-  });
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    });
 }
